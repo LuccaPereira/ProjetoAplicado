390cfc9a6d5c168fdfa5c4c2640fde1dd889ea3f
LuccaPereira <lucca.battaglin.2001@gmail.com>
2024-04-10 21:52:24 -0300

    Uptdate 04/10 login menu login html menu html deletado CPF php RUN

--- Projeto/Controller/login.controller.js
+++ Projeto/Controller/login.controller.js
@@ -1,16 +1,42 @@
-function getLawyers() {
+function getLawyerByOAB(OAB) {
     const databaseURL = "https://projetoaplicado-1-default-rtdb.firebaseio.com/";
     const collectionPath = "Advogado";
-    const url = `${databaseURL}/${collectionPath}.json`;
+    const url = `${databaseURL}/${collectionPath}/${OAB}.json`;
 
-    axios.get(url)
-        .then(response => {
-            const lawyers = response.data;
-            console.log("Advogados:", lawyers);
-        })
-        .catch(error => {
-            console.error("Erro ao obter advogados:", error);
-        });
+    return axios.get(url);
 }
 
-getLawyers();
+function login() {
+    var oab = document.getElementById("OAB").value;
+    var senha = document.getElementById("senha").value;
+
+    getLawyerByOAB(oab)
+        .then(response => {
+            const lawyerData = response.data;
+            console.log("Dados do advogado:", lawyerData);
+
+            var lawyerArray = Object.values(lawyerData);
+            var posicaoAdvogado = lawyerArray.find(advoGado => {
+                var position = advoGado.OAB;
+                if (oab === position) {
+                    for (var key in advoGado) {
+                        if (advoGado.senha === senha) {
+                            window.location.href = "../View/menu.html";
+                        } else {
+                            alert("Senha digitada esta errada")
+                            return;
+                        }
+                    } 
+                } else{
+                    alert("OAB está incorreta ou você não esta cadastrado!");
+                    return;  
+                } 
+            });
+            console.log(posicaoAdvogado);
+        })
+        .catch(error => {
+            console.error("Erro ao obter dados do advogado:", error);
+        });
+
+    event.preventDefault();
+}
