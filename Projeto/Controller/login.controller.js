3dd264cac3581754c7a95980573e0ca2e9a42d4b
Murilo-rgb <muritrevis@gmail.com>
2024-08-22 17:40:11 -0300

    mudança no controller cadastrar petição

--- Projeto/Controller/login.controller.js
+++ Projeto/Controller/login.controller.js
@@ -48,22 +48,22 @@ function login() {
     var oab = document.getElementById("OAB").value;
     var senha = document.getElementById("senha").value;
 
-    if(validarCPF(oab)) {
+    if (validarCPF(oab)) {
         getCpfByCliente(oab)
             .then(response => {
                 const cliente = response.data;
-                console.log("Dados do advogado:", cliente);
-    
+                console.log("Dados do cliente:", cliente);
+
                 if (!cliente) {
                     alert("Você não está cadastrado ou seu CPF está incorreto");
                     return;
                 }
-    
+
                 var clienteArray = Object.values(cliente);
                 var posicaoCliente = clienteArray.find(clienteGado => {
                     var positionCpf;
                     var senhaClientela;
-    
+
                     if (clienteGado.NomeAdvogado) {
                         const adv = clienteGado.NomeAdvogado.toString();
                         positionCpf = clienteGado[adv].CPFAtivo;
@@ -72,35 +72,35 @@ function login() {
                         positionCpf = clienteGado.cpf;
                         senhaClientela = clienteGado.senha;
                     }
-    
+
                     if (oab === positionCpf) {
                         if (senha === senhaClientela) {
+                            localStorage.setItem('loggedInCliente', JSON.stringify(clienteGado));
                             window.location.href = "../View/consultar.html";
-                            return true; 
+                            return true;
                         } else {
                             alert("Senha está incorreta");
-                            return false; 
+                            return false;
                         }
                     }
-    
-                    return false; 
+
+                    return false;
                 });
-    
+
                 if (!posicaoCliente) {
                     alert("CPF não encontrado ou senha incorreta");
                 }
-    
+
                 console.log(posicaoCliente);
             })
-        .catch(error => {
-            console.error("Erro ao obter dados do cliente:", error);
-            alert("Ocorreu um erro ao processar sua solicitação. Tente novamente mais tarde.");
-        });
+            .catch(error => {
+                console.error("Erro ao obter dados do cliente:", error);
+                alert("Ocorreu um erro ao processar sua solicitação. Tente novamente mais tarde.");
+            });
     } else {
         console.log("CPF inválido");
     }
-    
-    
+
     getLawyerByOAB(oab)
         .then(response => {
             const lawyerData = response.data;
@@ -114,15 +114,20 @@ function login() {
             var lawyerArray = Object.values(lawyerData);
             var posicaoAdvogado = lawyerArray.find(advoGado => {
                 var position = advoGado.OAB;
-                if (oab === position){
-                    if(advoGado.senha === senha) {
+                if (oab === position) {
+                    if (advoGado.senha === senha) {
+                        localStorage.setItem('loggedInLawyer', JSON.stringify(advoGado));
                         window.location.href = "../View/menu.html";
+                        return true;
                     } else {
                         alert("Senha está incorreta");
-                        return;
+                        return false;
                     }
                 }
+
+                return false;
             });
+
             console.log(posicaoAdvogado);
         })
         .catch(error => {
