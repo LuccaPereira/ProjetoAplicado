315a3d0bf1710614073fd8c7ee13530de9969d78
LuccaPereira <lucca.battaglin.2001@gmail.com>
2024-09-18 18:23:10 -0300

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import "../public/View/loginCss.css";
 import "../public/Controller/login.controller";
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa o JavaScript do Bootstrap
