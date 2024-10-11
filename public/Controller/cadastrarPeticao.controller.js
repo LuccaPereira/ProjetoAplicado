35e512919495d375dd7c0d245b641c828173114b
LuccaPereira <lucca.battaglin.2001@gmail.com>
2024-10-11 13:47:17 -0300

    versão nova do css

--- public/Controller/cadastrarPeticao.controller.js
+++ public/Controller/cadastrarPeticao.controller.js
@@ -1,14 +1,10 @@
 import { montarOData } from '../model/cadastrarPeticao.js';
 
-
-document.getElementById('Click').addEventListener('click', function() {
+document.getElementById('menuToggle').addEventListener('click', function() {
     const sidebar = document.querySelector('.sidebar');
-    const menuToggle = document.getElementById('menuToggle');
-    
-    menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('expanded');
-    });
+    sidebar.classList.toggle('expanded');
 });
+
 
 document.getElementById('logoutButton').addEventListener('click', function() {
     localStorage.removeItem('loggedInLawyer');
@@ -17,7 +13,7 @@ document.getElementById('logoutButton').addEventListener('click', function() {
     window.location.href = '../View/login.html';
 })
 
-document.getElementById('loginBnt').addEventListener('click', function() {
+document.getElementById('loginButton').addEventListener('click', function() {
     window.location.href = '../View/perfilAdvogado.html';
 });
 
