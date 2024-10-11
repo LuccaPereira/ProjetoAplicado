19a0de28aaadc71588aa560e84663338aa7cfe29
Murilo-rgb <muritrevis@gmail.com>
2024-10-10 21:21:54 -0300

    mudanças cadastro de petição e criar cliente

--- public/Controller/cadastrarPeticao.controller.js
+++ public/Controller/cadastrarPeticao.controller.js
@@ -1,33 +1,30 @@
 import { montarOData } from '../model/cadastrarPeticao.js';
 
 
-document.getElementById('Click').addEventListener('click', function() {
-    const sidebar = document.querySelector('.sidebar');
-    const menuToggle = document.getElementById('menuToggle');
-    
-    menuToggle.addEventListener('click', () => {
-        sidebar.classList.toggle('expanded');
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menuToggle')?.addEventListener('click', () => {
+        const sidebar = document.querySelector('.sidebar');
+        sidebar?.classList.toggle('expanded');
     });
-});
 
-document.getElementById('logoutButton').addEventListener('click', function() {
-    localStorage.removeItem('loggedInLawyer');
-    localStorage.removeItem('loggedInCliente');
+    document.getElementById('logoutButton')?.addEventListener('click', function() {
+        localStorage.removeItem('loggedInLawyer');
+        localStorage.removeItem('loggedInCliente');
+        window.location.href = '../View/login.html';
+    });
 
-    window.location.href = '../View/login.html';
-})
+    document.getElementById('loginBnt')?.addEventListener('click', function() {
+        window.location.href = '../View/perfilAdvogado.html';
+    });
 
-document.getElementById('loginBnt').addEventListener('click', function() {
-    window.location.href = '../View/perfilAdvogado.html';
-});
-
-document.getElementById('envio').addEventListener('click', function() {
-    montarOData()
-        .then(() => {
-            alert("Novo cliente cadastrado com sucesso!");
-            window.location.reload();
-        })
-        .catch(error => {
-            alert(`Erro: ${error}`);
-        });
+    document.getElementById('envio')?.addEventListener('click', function() {
+        montarOData()
+            .then(() => {
+                alert("Novo cliente cadastrado com sucesso!");
+                window.location.reload();
+            })
+            .catch(error => {
+                alert(`Erro: ${error}`);
+            });
+    });
 });
