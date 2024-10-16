cd7f0b60c276cb2c2d8fa3ecda507a6640cf4cf5
Regizzera <bielregiscunha1@gmail.com>
2024-10-16 20:05:26 -0300

    boa

--- public/Controller/cadastrarPeticao.controller.js
+++ public/Controller/cadastrarPeticao.controller.js
@@ -1,35 +1,6 @@
 import { montarOData } from '../model/cadastrarPeticao.js';
 
-<<<<<<<<< Temporary merge branch 1
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('menuToggle')?.addEventListener('click', () => {
-        const sidebar = document.querySelector('.sidebar');
-        sidebar?.classList.toggle('expanded');
-    });
-
-    document.getElementById('logoutButton')?.addEventListener('click', function() {
-        localStorage.removeItem('loggedInLawyer');
-        localStorage.removeItem('loggedInCliente');
-        window.location.href = '../View/login.html';
-    });
-
-    document.getElementById('loginBnt')?.addEventListener('click', function() {
-        window.location.href = '../View/perfilAdvogado.html';
-    });
-
-    document.getElementById('envio')?.addEventListener('click', function() {
-        montarOData()
-            .then(() => {
-                alert("Novo cliente cadastrado com sucesso!");
-                window.location.reload();
-            })
-            .catch(error => {
-                alert(`Erro: ${error}`);
-            });
-    });
-});
-=========
 document.getElementById('menuToggle').addEventListener('click', function() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('expanded');
@@ -57,4 +28,3 @@ document.getElementById('envio').addEventListener('click', function() {
             alert(`Erro: ${error}`);
         });
 });
->>>>>>>>> Temporary merge branch 2
